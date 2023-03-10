3671fe96bf636992ad193336e9892dc9e8869559
kitoshi <kitoshi.charlton@gmail.com>
2023-03-10 09:25:44 -0800

    sorted with column click

--- app/components/Table.tsx
+++ app/components/Table.tsx
@@ -1,24 +1,103 @@
-import { Children } from 'react';
+import { Children, useEffect, useState } from 'react';
 import { CrawlerAPIObject } from '../data/ApiData';
+import styles from './Table.module.css';
 
 interface Props {
   data: CrawlerAPIObject[] | undefined;
 }
 
+interface CalculatedCrawlerAPIObject extends CrawlerAPIObject {
+  priceDifference?: number | string;
+}
+
+enum ColumnName {
+  ItemName = 'itemName',
+  ItemPrice = 'gcPrice',
+  EbayPrice = 'ebayPrice',
+  PriceDifference = 'priceDifference'
+}
+
 export default function Table({ data }: Props) {
+  const [tableData, setTableData] = useState<
+    CalculatedCrawlerAPIObject[] | undefined
+  >(data);
+  const [sorted, setSorted] = useState(false);
+
+  useEffect(() => {
+    if (data) {
+      const updatedTableData = data.map((item) => {
+        const gcPrice = parseFloat(item.gcPrice.replace(/[$,]/g, ''));
+        const ebayPrice = parseFloat(item.ebayPrice.replace(/[$,]/g, ''));
+        let priceDifference = (gcPrice - ebayPrice).toFixed(2);
+        if (isNaN(Number(priceDifference))) {
+          priceDifference = 'N/A';
+        }
+        return {
+          ...item,
+          priceDifference: '$' + priceDifference // Round to 2 decimal places
+        };
+      });
+      setTableData(updatedTableData);
+    }
+  }, [data]);
+
+  function sortByColumn(column: ColumnName) {
+    if (tableData) {
+      const sortedTableData = [...tableData].sort((itemA, itemB) => {
+        setSorted(true);
+        if (column === ColumnName.ItemName) {
+          return itemA.itemName.localeCompare(itemB.itemName);
+        }
+        if (column === ColumnName.ItemPrice) {
+          return (
+            parseFloat(itemA.gcPrice.replace(/[$,]/g, '')) -
+            parseFloat(itemB.gcPrice.replace(/[$,]/g, ''))
+          );
+        }
+        if (column === ColumnName.EbayPrice) {
+          return (
+            parseFloat(itemA.ebayPrice.replace(/[$,]/g, '')) -
+            parseFloat(itemB.ebayPrice.replace(/[$,]/g, ''))
+          );
+        }
+        if (column === ColumnName.PriceDifference) {
+          const priceDiffA = parseFloat(
+            itemA.priceDifference?.toString().replace(/[$,]/g, '') || '0'
+          );
+          const priceDiffB = parseFloat(
+            itemB.priceDifference?.toString().replace(/[$,]/g, '') || '0'
+          );
+          return priceDiffA - priceDiffB;
+        }
+        {
+          return 0;
+        }
+      });
+      if (sorted) {
+        sortedTableData.reverse();
+        setSorted(false);
+      }
+      setTableData(sortedTableData);
+    }
+  }
+
   return (
-    <table>
+    <table className={styles.table}>
       <thead>
         <tr>
-          <th>Item Name</th>
-          <th>Item Price</th>
-          <th>Ebay Price</th>
-          <th>Price Difference</th>
+          <th onClick={() => sortByColumn(ColumnName.ItemName)}>Item Name</th>
+          <th onClick={() => sortByColumn(ColumnName.ItemPrice)}>
+            Government Price
+          </th>
+          <th onClick={() => sortByColumn(ColumnName.EbayPrice)}>Ebay Price</th>
+          <th onClick={() => sortByColumn(ColumnName.PriceDifference)}>
+            Price Difference
+          </th>
         </tr>
       </thead>
       <tbody>
         {Children.toArray(
-          data?.map((item) => (
+          tableData?.map((item) => (
             <tr>
               <td>{item.itemName}</td>
               <td>
@@ -27,7 +106,7 @@ export default function Table({ data }: Props) {
               <td>
                 <a href={item.ebayLink}>{item.ebayPrice}</a>
               </td>
-              <td>quick maths</td>
+              <td>{item.priceDifference}</td>
             </tr>
           ))
         )}
